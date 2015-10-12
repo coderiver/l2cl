3664e093b8cb8c0442b1ca645f398d065434c53f
Maryna Kozelchuk <marina.kozelchuk@gmail.com>
2015-10-12 14:40:32 +0300

    fixes

--- src/js/common.js
+++ src/js/common.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
 
 	$('.js-loc-btn').click(function() {
-		// if ($(this).hasClass('is-active')){
-		// 	$('.js-loc-btn').removeClass('is-active');
-		// 	$('.js-location').removeClass('is-active');
-		// }
-		// else {
-		// 	$('.js-loc-btn').addClass('is-active');
-		// 	if ($('.js-location').hasClass('is-active')) {
-		// 		$('.js-location').removeClass('is-active');
-		// 	}
-		// 	else {
-		// 		$('.js-location').addClass('is-active');
-		// 	}
-		// }
 		$('.js-loc-btn').addClass('is-active');
 		$('.js-location').addClass('is-active is-index');
 		$('.js-weapon').removeClass('is-index');
@@ -24,20 +11,6 @@ $(document).ready(function() {
 	});
 
 	$('.js-weapon-btn').click(function() {
-		// if ($(this).hasClass('is-active')){
-		// 	$('.js-weapon-btn').removeClass('is-active');
-		// 	$('.js-weapon').removeClass('is-active');
-		// }
-		// else {
-		// 	$('.js-weapon-btn').addClass('is-active');
-		// 	if ($('.js-weapon').hasClass('is-active')) {
-		// 		$('.js-weapon').removeClass('is-active');
-		// 	}
-		// 	else {
-		// 		$('.js-weapon').addClass('is-active');
-		// 	}
-		// }
-		
 		$('.js-weapon-btn').addClass('is-active');
 		$('.js-weapon').addClass('is-active is-index');
 		$('.js-location').removeClass('is-index');
